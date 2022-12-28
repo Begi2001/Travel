fc24e865d770a6e62110fd8e56dd035abf697711
Bek8866 <begi20010202@gmail.com>
2022-12-28 15:56:09 +0500

    filter2

--- src/components/Filters2/Filter2.jsx
+++ src/components/Filters2/Filter2.jsx
@@ -188,6 +188,47 @@ const Filter2 = () => {
                   </div>
                 );
               }
+              {
+                <div key={res.id} className="WraperMap">
+                  <img alt="" src={BigImg} />
+                  <div className="Tours">
+                    <div>
+                      <p className="resType">{res.type}</p>
+                      <p className="resTitle">{res.title}</p>
+                    </div>
+                    <div className="serviceDiv">
+                      <div className="moneyDiv">
+                        <div className="leftDiv">
+                          <div className="topLeft">
+                            <img alt="" src={Coin} />
+                            <p className="price">{res.moneyAdult}</p>
+                          </div>
+                          <p className="ticket">Взрослый билет</p>
+                        </div>
+                        <div className="RightDiv">
+                          <div className="topRight">
+                            <img alt="" src={Coin} />
+                            <p>{res.moneyChild}</p>
+                          </div>
+                          <p className="ticket">Детский билет</p>
+                        </div>
+                      </div>
+                      <div className="timeDiv">
+                        <img alt="" className="timePng" src={Time} />
+                        <p>{res.clock}</p>
+                      </div>
+                    </div>
+                    <div className="hrAbout">
+                      <hr />
+                      <p>{res.description}</p>
+                    </div>
+                    <div className="btnLike">
+                      <button className="btnInfo">Подробнее</button>
+                      <img className="likeImg" src={Like} alt="" />
+                    </div>
+                  </div>
+                </div>;
+              }
             })}
           </div>
         </div>
